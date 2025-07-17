630063b715beb1638bf65abc07e25c738856bc24
DiegoFatVega <fatvegadiego@gmail.com>
2025-07-17 23:34:31 +0200

    Refactor function to extract initials from names array

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+ Array_di_iniziali = [];
 
+function prendi_iniziali (array){
+    for(let i = 0; i < names.length; i ++){
+        const name = array[i];
+        Array_di_iniziali.push(name.charAt(0));
+    }
+    return Array_di_iniziali;
+}
+
+const iniziali = prendi_iniziali(names);
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(iniziali);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
