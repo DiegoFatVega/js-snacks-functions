bf9ca98731fc51e90abe94df4d08c375a519da81
DiegoFatVega <fatvegadiego@gmail.com>
2025-07-18 08:31:30 +0200

    Refactor countVowels function to correctly count vowels in the input string

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(string) {
+    const vowels = 'aeiou'
+    let count = 0;
 
-
+    for(let i = 0; i < word.length; i++){
+        if(vowels.includes(string[i])){
+            count ++;
+        }
+    }
+    return count;
+}
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
